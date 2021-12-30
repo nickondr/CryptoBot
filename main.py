6b424194c46fbab1b6167cd09e01235cda414c12
nickondr <nondr@falcon.bentley.edu>
2021-12-29 21:50:36 -0600

    fourth update - The program is fully functioning now. There is currently an issue regarding saving the workspace data in the database.py file regarding sqlite3. Help on this issue would be appreciated. The program is now able to make trades automatically using stop loss and take profit protocols.

--- main.py
+++ main.py
@@ -22,9 +22,8 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':
 
-    binance = BinanceFuturesClient("5680cfa75f5bb50033912afa39fae9bb752d6aabec1789c13ac183748a621522",
-                                   "5d04972eabef4bb2fb632a65a6ae46b4f9c0b8a2075517ab6a04fd202ff3fa21", True)
-    bitmex = BitmexClient("FLtRBQiE4LOTxgQ8HIadPszE", "GDhltmEL10PDHnCdC8rRc7phw9hW5cMCMV87vGInFLUV_BW8", True)
+    binance = BinanceFuturesClient("", "", True)
+    bitmex = BitmexClient("", "", True)
 
     root = Root(binance, bitmex)
     root.mainloop()
